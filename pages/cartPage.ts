4043183f290f563c56c0fdc9de87bbb9b9de3018
volk314 <anastasiia.volkova@outlook.com>
2023-04-25 23:50:04 +0200

    - console log

--- pages/cartPage.ts
+++ pages/cartPage.ts
@@ -18,10 +18,8 @@ class CartPage {
         I.click(productPage.cartPreviewButton);
         if (await I.grabNumberOfVisibleElements(productPage.cartButton)){
             I.click(productPage.cartButton);
-            console.log(await I.grabNumberOfVisibleElements(this.deleteButton));
             while(await I.grabNumberOfVisibleElements(this.deleteButton)){
                 I.click(this.deleteButton);
-                console.log(await I.grabNumberOfVisibleElements(this.deleteButton));
             }
         }
     }
