213782b2dc19ab884fce981d7efb5e2d60f0b158
volk314 <anastasiia.volkova@outlook.com>
2023-04-25 23:40:55 +0200

    uncomented row

--- tests/purchase_test.ts
+++ tests/purchase_test.ts
@@ -32,7 +32,7 @@ Data(products).Scenario('Buy 1 product',  async ({ I, current }) => {
     await I.see("My Cart");
     if (await I.grabNumberOfVisibleElements(cartPage.productUnavailableLabel)){
         console.log("This product is unavailable for purchase, we will clear the cart now.");
-        //CartPage.clearCart();
+        CartPage.clearCart();
     } else {
         await CartPage.proceedToCheckout();
         let checkoutPrice: number = await CheckoutPage.placeOrder();
