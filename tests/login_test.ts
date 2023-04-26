0a87fea6a0f18659316feabc4c96af876e6321cc
volk314 <anastasiia.volkova@outlook.com>
2023-04-26 13:57:24 +0200

    await

--- tests/login_test.ts
+++ tests/login_test.ts
@@ -3,13 +3,13 @@ import LoginPage = require("../pages/loginPage");
 
 Feature('Login');
 
-Scenario('Login',  ({ I }) => {
+Scenario('Login',  async ({ I }) => {
 
     const USER: User = {
         email: `anastasiia0.2687341244603123@mailinator.com`,
         password: "50p8c0ze"
     };
 
-    LoginPage.login(USER);
+    await LoginPage.login(USER);
     I.see('My Affiliate Account');
 });
